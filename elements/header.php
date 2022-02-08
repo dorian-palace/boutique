5cf68707bf55494afd9eeaca171f72e4ee50ca3e
tony-guillot <tony.guilot@laplateforme.io>
2022-02-08 15:24:15 +0100

    header.php

---
+++ elements/header.php
@@ -0,0 +1,24 @@
+
+<nav>
+      <div class="dropdown">
+        <button><a href="#" class="home">Accueil</a></button>
+        <div class="projects">
+          <button>Projects</button>
+          <ul>
+            <li><a href="#">Cat1</a></li>
+            <li><a href="#">cat2</a></li>
+            <li><a href="#">cat3</a></li>
+            <li><a href="#">cat4</a></li>
+          </ul>
+        </div>
+        <div class="products">
+          <button>Mon compte</button>
+          <ul>
+            <li><a href="#">Modifier mon profil</a></li>
+            <li><a href="#">test</a></li>
+            <li><a href="#">test</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
