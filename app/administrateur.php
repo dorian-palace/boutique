1eeaefd9b5a041d2280eb7d32fd0870a4493ee9c
dorian-palace <dorian.palace@laplateforme.io>
2022-02-10 20:04:40 +0100

    admin

---
+++ app/administrateur.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../setting/db.php');
+
+class Admin
+{
+
+    public function __construct()
+    {
+        $this->db = new Db_connect();
+        $this->db = $this->db->return_connect();
+    }
+
+    public function gestion_stock()
+    {
+        $req = ('SELECT * FROM produits ')
+    }
+}
