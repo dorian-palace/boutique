26c4ca1e1257ef0bee60c36dcadc1b397cf91816
dorian-palace <dorian.palace@laplateforme.io>
2022-02-11 16:51:36 +0100

    method get_user/delete

--- admin.php
+++ admin.php
@@ -1,9 +1,21 @@
 <?php
 session_start();
 require('app/administrateur.php');
+$user = new Administrateur();
 
+$var = $user->get_User();
+var_dump($var);
+$user->get_User();
+
+if (isset($_GET['delete']) && !empty($_GET['delete'])) {
+    $delete = (int)$_GET['delete'];
+    $user->delete_User($delete);
+}
+
+$user->get_User();
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,8 +29,34 @@ require('app/administrateur.php');
 <body>
     <!--- HEADER --->
     <main>
-        <aside>
+        <ul>
+            <li>
+                <!--- $azd = $user->get_User puis while .... ---->
+                login : <?= $user->get_User()['login']; ?>
+            </li>
+            <li> email: <?= $user->get_User()['email'] ?></li>
+            <li> password :<?= $user->get_User()['password'] ?></li>
+            <li> id : <?= $user->get_User()['id'] ?></li>
+            <li> id_droits :<?= $user->get_User()['id_droits'] ?></li>
+            <li> id_adresse :<?= $user->get_User()['id_adresse'] ?></li>
 
+        </ul>
+
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, voluptatibus libero magnam iusto quam repudiandae enim, sequi totam omnis, doloremque molestiae inventore modi nisi. Ut et doloremque provident ab est.
+
+        <form action="admin.php" method="post">
+            <fieldset>
+                <legend>suppression utilisateur</legend>
+                <input type="text" value="<?= $user->get_User()['login']; ?>">
+                <input type="email" value="<?= $user->get_User()['email']; ?>">
+                <input type="password" value="<?= $user->get_User()['password']; ?>">
+                <input type="submit" value="suppression">
+                <a class="a_admin" href="admin.php?delete=<?= $user->get_User()['id'] ?>">Supprimer</a>
+            </fieldset>
+        </form>
+
+        <aside>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit hic dolor esse veniam consequuntur expedita veritatis natus accusamus corporis, dignissimos aliquam repudiandae qui neque unde sed recusandae eaque itaque commodi.</p>
         </aside>
     </main>
     <!--- FOOTER --->
--- app/administrateur.php
+++ app/administrateur.php
@@ -1,7 +1,7 @@
 <?php
-require_once('../setting/db.php');
+require_once('setting/db.php');
 
-class Admin
+class Administrateur
 {
 
     public function __construct()
@@ -10,8 +10,40 @@ class Admin
         $this->db = $this->db->return_connect();
     }
 
-    public function gestion_stock()
+    public function get_User()
+    {
+        $req = 'SELECT * FROM utilisateurs';
+        $query = $this->db->query($req);
+        $fetch = $query->fetch();
+        return $fetch;
+    }
+
+    public function delete_User($delete)
+    {
+
+        $req = 'DELETE FROM utilisateurs WHERE id = ?';
+        $prepare = $this->db->prepare($req);
+        $prepare->execute(array(
+            $delete
+        ));
+        return $prepare;
+        $msg = 'Utilisateurs supprimer';
+    }
+
+    public function upgrade_Droits()
+    {
+    }
+
+    public function gestion_Stock()
     {
         $req = ('SELECT * FROM produits ');
+    }
+
+    public function new_Categorie()
+    {
+    }
+
+    public function upgrade_Categorie()
+    {
     }
 }
