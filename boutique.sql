f0449fccf473837609e9b0f4c5aa9f2fee52b7c9
grassi-andre <andre.grassi@laplateforme.io>
2022-04-11 16:56:49 +0200

    index ect

--- boutique.sql
+++ boutique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mer. 09 fév. 2022 à 17:19
+-- Généré le : jeu. 10 mars 2022 à 11:51
 -- Version du serveur : 5.7.34
 -- Version de PHP : 8.0.8
 
@@ -66,6 +66,16 @@ CREATE TABLE `categories` (
   `nom_categorie` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `nom_categorie`) VALUES
+(38, 'mcdo'),
+(39, 'Catégorie test admin'),
+(40, 'kkkazekaze'),
+(42, 'new test');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +86,14 @@ CREATE TABLE `droits` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `droits`
+--
+
+INSERT INTO `droits` (`id`, `nom`) VALUES
+(1, 'utilisateur'),
+(13, 'admin');
 
 -- --------------------------------------------------------
 
@@ -92,10 +110,10 @@ CREATE TABLE `facture` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `liste_commande`
+-- Structure de la table `panier`
 --
 
-CREATE TABLE `liste_commande` (
+CREATE TABLE `panier` (
   `id` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `id_produit` int(11) NOT NULL,
@@ -117,8 +135,22 @@ CREATE TABLE `produits` (
   `stock` int(11) NOT NULL,
   `id_categorie` int(11) NOT NULL,
   `id_regions` int(11) NOT NULL,
-  `prix` float NOT NULL
+  `prix` float NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT INTO `produits` (`id`, `titre`, `description`, `stock`, `id_categorie`, `id_regions`, `prix`, `image`) VALUES
+(15, 'test italiee', 'italiee', 123222, 39, 5, 20, '21dEPPiDp3L._AC_SX425_.jpg'),
+(16, 'a', 'a', 1, 38, 2, 1, '21dEPPiDp3L._AC_SX425_.jpg'),
+(17, 'ijzaeieazioj', 'io', 3232, 38, 2, 122, 'w_sun_pgold.png'),
+(18, '1112212', 'aze', 321132, 40, 5, 122, 'téléchargement.png'),
+(19, 'nnnnn', 'nnnn', 23123, 38, 2, 123123, 'image_NFT_auto_x2_light_ai20.jpeg'),
+(20, 'uuuzuzuzu', 'fghdj', 23123, 38, 2, 123123, 'w_sun_pgoold.png'),
+(21, 'kdkdnazdn', 'kkakaka', 2, 38, 2, 41, 'w_sun_pink.png');
 
 -- --------------------------------------------------------
 
@@ -128,8 +160,17 @@ CREATE TABLE `produits` (
 
 CREATE TABLE `regions` (
   `id` int(11) NOT NULL,
-  `nom_region` int(11) NOT NULL
+  `nom_region` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `regions`
+--
+
+INSERT INTO `regions` (`id`, `nom_region`) VALUES
+(2, 'a'),
+(4, 'marseille'),
+(5, 'Regions admin');
 
 -- --------------------------------------------------------
 
@@ -147,6 +188,13 @@ CREATE TABLE `utilisateurs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `id_adresse`, `id_droits`, `login`, `password`, `email`) VALUES
+(1, 1, 1, 'testaaa', 'test', 'test@test.fr');
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -154,13 +202,17 @@ CREATE TABLE `utilisateurs` (
 -- Index pour la table `adresse_livraison`
 --
 ALTER TABLE `adresse_livraison`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gfnffjnh` (`id_utilisateur`);
 
 --
 -- Index pour la table `avis`
 --
 ALTER TABLE `avis`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_avis_categorie` (`id_categorie`),
+  ADD KEY `fk_utilisateur_id` (`id_utilisateur`),
+  ADD KEY `fk_produits_id` (`id_produit`);
 
 --
 -- Index pour la table `categories`
@@ -172,19 +224,25 @@ ALTER TABLE `categories`
 -- Index pour la table `facture`
 --
 ALTER TABLE `facture`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_utilisateur` (`id_utilisateur`),
+  ADD KEY `fk_commande` (`numero_commande`);
 
 --
--- Index pour la table `liste_commande`
+-- Index pour la table `panier`
 --
-ALTER TABLE `liste_commande`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_produits` (`id_produit`),
+  ADD KEY `panier_ibfk_1` (`id_utilisateur`);
 
 --
 -- Index pour la table `produits`
 --
 ALTER TABLE `produits`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_categorie` (`id_categorie`),
+  ADD KEY `fk_regions` (`id_regions`);
 
 --
 -- Index pour la table `regions`
@@ -218,7 +276,7 @@ ALTER TABLE `avis`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT pour la table `facture`
@@ -227,28 +285,61 @@ ALTER TABLE `facture`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `liste_commande`
+-- AUTO_INCREMENT pour la table `panier`
 --
-ALTER TABLE `liste_commande`
+ALTER TABLE `panier`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `produits`
 --
 ALTER TABLE `produits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT pour la table `regions`
 --
 ALTER TABLE `regions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD CONSTRAINT `fk_avis_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `fk_produits_id` FOREIGN KEY (`id_produit`) REFERENCES `produits` (`id`),
+  ADD CONSTRAINT `fk_utilisateur_id` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
+
+--
+-- Contraintes pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD CONSTRAINT `fk_commande` FOREIGN KEY (`numero_commande`) REFERENCES `panier` (`id`),
+  ADD CONSTRAINT `fk_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `fk_produits` FOREIGN KEY (`id_produit`) REFERENCES `produits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `fk_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_regions` FOREIGN KEY (`id_regions`) REFERENCES `regions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/css/header.css">
+    <link rel="stylesheet" href="style/css/index.css">
     <title>Document</title>
 </head>
 
@@ -21,12 +22,32 @@
     <main>
         <!--- MAIN --->
 
+        <h1 class="titre">
+            Pasta di Giovanni 
+        </h1>
+
     </main>
 
-    <article>
+    <article class="presentation">
         <!--- ARTICLE --->
+        <div class="textpresentation"></div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo soluta consectetur voluptates tempora dolore, tenetur repudiandae cupiditate rerum reprehenderit maiores beatae fuga, quos id sapiente fugit debitis. Amet, quia consectetur!
+        </div>
+
+        
 
     </article>
+
+    
+
+    <div class="produitaccueil">
+
+        <?php
+           $req =  "SELECT * FROM produits ORDER BY DESC LIMIT 5 ";
+        ?>
+
+
+    </div>
 
 
     <!--- FOOTER --->
