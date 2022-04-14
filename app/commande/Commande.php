b10f5f2e241df0bcacf3fe4e9a716e4edbb4f079
dorian-palace <dorian.palace@laplateforme.io>
2022-04-14 18:29:09 +0200

    insert commande

--- app/commande/Commande.php
+++ app/commande/Commande.php
@@ -10,5 +10,21 @@ class Commande
         $this->db = $this->db->return_connect();
     }
 
-    
+    public function validationCommande($id_user)
+    {
+        if (isset($_POST['adr_fact']) && isset($_POST['adr_liv'])) {
+
+            $facture = $_POST['adr_fact'];
+            $livraison = $_POST['adr_liv'];
+            $id_user = $_SESSION['id'];
+
+            //INSERT INTO commande (adr_facturation, adr_livraison, id_utilisateur, date_commande) VALUES (?,?,?, NOW)
+            $req = " INSERT INTO commande (adr_facturation, adr_livraison, id_utilisateur, date_commande, statut) VALUES ('$facture','$livraison','$id_user',NOW(), 0)";
+            $stmt = $this->db->query($req);
+            // $stmt->execute(array(
+            //     $facture, $livraison, $id_user
+            // ));
+            return $stmt;
+        }
+    }
 }
--- commande.php
+++ commande.php
@@ -1,16 +1,33 @@
 <?php
 session_start();
 require('app/commande/Commande.php');
+$commande = new Commande();
+$id_user = $_SESSION['id'];
+echo "<pre>";
+var_dump($_SESSION);
+echo "</pre>";
+
+if (isset($_POST['submit'])) {
+    $commande->validationCommande($id_user);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commande</title>
 </head>
+
 <body>
-    
+    <form action="" method="post" name="commandeForm">
+        <input type="text" placeholder="adresse de facturation" name="adr_fact">
+        <input type="text" placeholder="adresse de livraison" name="adr_liv">
+        <input type="submit" name="submit">
+    </form>
 </body>
+
 </html>
