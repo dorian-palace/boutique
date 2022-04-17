4deee797d1f859ad64cdc179089ea535a940cc8a
grassi-andre <andre.grassi@laplateforme.io>
2022-04-17 20:34:52 +0200

    css index en cour

--- index.php
+++ index.php
@@ -10,12 +10,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/css/header.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="style/css/index.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="pexel/PastadiGiovanni1.png">
+    <title>Accueil</title>
 </head>
 
-<body id="imgfond1">
+<body>
+    
 
-    <div>
+    
         <header>
             <?php require 'elements/header.php'; ?>
         </header>
@@ -28,17 +31,34 @@
 
         </main>
 
+        <h1 id="h1articleindex">
+            L'histoire de Pasta di Giovanni
+        </h1>
+
+    <div id="articleindex">
         <article>
             <!--- ARTICLE --->
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, eos minima expedita distinctio accusantium veritatis, soluta molestias omnis iure beatae iste optio quia quam. Harum vel eum illum rerum quae.
-        </article>
+            Pasta di Giovanni fabrique artisanalement des pâtes fraîches et farcies est surtout connue pour ses différentes spécialités de gnocchis et raviolis frais, dont la plus ancienne remonte à 1990.
 
+            Nous avons su garder un caractère artisanal (multiplicité des formats et farces), et évoluer en acquérant des outils de production moderne.
+
+            Ambassadeur de choix de la gastronomie locale, la marque “Pâtes fraîches et farcies” fleurie sur les cartes des grands restaurants et également dans les grands noms d’épicerie fine. Cette grande marque a su s’imposer comme la référence en pâtes fraîches originales et de qualité.
+
+            Tant les raviolis que les pâtes fraîches sont composés : de pâtes à base de farine de blé et de semoule extra fine ainsi que des différentes farces conçues par nous-même. Cette composition lui donne une particularité qui réside de la rapidité de cuisson : 3 minutes dans l’eau bouillante, feu doux.
+
+            La fabrication est effectuée chaque jour sauf le lundi (jour de fermeture de notre fabrique artisanale) afin de conserver la fraîcheur des produits, nous vous assurons une date limite de consommation de nos produits de 6 jours, car nous n’utilisons pas de produit conservateur.
+
+            Nous vous laissons découvrir nos spécialités et produits de tradition et vous attendons dans notre magasin pour vous présenter les produits régionaux et de saison.
+
+            Traitez vos papilles comme il se doit…
+        </article>
+     </div>
 
         <!--- FOOTER --->
         <footer>
             <?php require 'elements/footer.html'; ?>
         </footer>
-    </div>
+    
 </body>
 
 </html>
